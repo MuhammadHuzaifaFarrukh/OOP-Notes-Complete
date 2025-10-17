15cb73d19271cafde94aa2412387da3aa66574a1
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2025-10-17 21:15:11 +0500

    Update main.cpp

--- 12) Constructor Overloading/main.cpp
+++ 12) Constructor Overloading/main.cpp
@@ -42,9 +42,10 @@ void Complex::printdata()
 }
 int main()
 {
-    Complex c1 = Complex(4,6);
-    // c1 = Complex(3,6); // Explicit Call when changing values
-    // Implicit Call will not work for this purpose
+    Complex c1 = Complex(4,6); //(Copy Epsilon , object b is constructed directly by calling the parameterized constructor in its final memory using the arg 4 and 6)
+    // c1 = Complex(3,6); // (Use this only when you don't have any dynamically allocated data members , as it calls constructor and fills data into a new object and then calls compiler generated copy assignment operator which is never good when we have dynamically allocated data members)
+    //Remember Constructor call is only done when object is created , not after its creation , as its done above , the created object above is a temporary created object which is then copied using compiler generated copy assingment code
+    //Although compiler uses Copy Epsilon Method here for optimization , we may think as parameterized constructor and compiler generated copy constructor called here , though it did happen in older C++ versions)
     c1.printdata();
 
     Complex c2(5);
