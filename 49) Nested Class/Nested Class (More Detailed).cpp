23babbeb485b0d3b264d060d890d7cb10e0a7d05
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2025-12-01 22:09:32 +0500

    Update Nested Class (More Detailed).cpp

--- 49) Nested Class/Nested Class (More Detailed).cpp
+++ 49) Nested Class/Nested Class (More Detailed).cpp
@@ -1,92 +1,76 @@
 #include <iostream>
+
 using namespace std;
 
-class University
+//Defining one class inside another is called Nested Class
+
+class A
 {
-private:
-    string universityName;
-    int establishedYear;
+    int a = 1;
+public :
 
-    // ✅ Composition: University CONTAINS Department objects
-    class Department
-    {
-    private:
-        string deptName;
-        int budget;
 
-        // Friendship for bidirectional access
-        friend class University;
+    void fun();
+// private:  //If we make class B private , then we need to call member functions of class B objects through nesting member functions of class A objects.
+    class B; // Can be forward declared like this.
 
-    public:
-        Department(string name, int bg) : deptName(name), budget(bg)
-        {
-            cout << "Department " << deptName << " created\n";
-        }
 
-        ~Department()
-        {
-            cout << "Department " << deptName << " destroyed\n";
-        }
+    // You may also create an object of class B here .
+    //  B b2; // Now when we use class A , we already have an object of class B.
+    //If you make an object of inner class in outer class , the accessiblity changes and you will use the inner class' member functions using nested member functions of outer class.
+    //You cannot call the inner member function from main() by declaring an object here below
 
-        void display() const
-        {
-            cout << "Dept: " << deptName << ", Budget: " << budget << endl;
-        }
-    };
+    //class B{   };
 
-    // University composition: has multiple departments
-    Department csDept;
-    Department eeDept;
-
-public:
-    University(string name, int year)
-        : universityName(name), establishedYear(year),
-          csDept("Computer Science", 500000),
-          eeDept("Electrical Engineering", 400000)
-    {
-        cout << "University " << universityName << " created\n";
-    }
-
-    ~University()
-    {
-        cout << "University " << universityName << " destroyed\n";
-        // ✅ Automatic: csDept and eeDept destroyed automatically
-    }
-
-    void displayUniversity()
-    {
-        cout << "\n=== " << universityName << " (Est: " << establishedYear << ") ===" << endl;
-
-        // ✅ Can access Department private members due to friendship
-        cout << "CS Dept Budget: " << csDept.budget << endl;
-        cout << "EE Dept Budget: " << eeDept.budget << endl;
-
-        csDept.display();
-        eeDept.display();
-    }
-
-    // Department can access University private data through methods
-    void letDepartmentsAccess()
-    {
-        cout << "\nDepartments accessing University data:" << endl;
-        // Simulating department accessing university data
-        cout << "University established: " << establishedYear << endl;
-    }
 };
+
+class A::B
+{
+    int a =6;
+public :
+    void display();
+
+
+} b2; //If nested class is defined here , then object of inner class cannot be inside the outer class. It is considered to be inside outer class.
+
+void A::B::display()
+{
+    A a5; // You can also make outer class objects inside inner class member functions and vice versa
+    std::cout<<"Hello, I'm the nested class B member function :) "<<a5.a<<endl; //Inner Class can use the private data members of outer class without being friends
+
+
+}
+void A::fun()
+{
+//    B b4;
+//    b4.a = 200;   //Outer Class cannot access inner class private data members without being friend
+//    cout<<b4.a<<endl; //Outer Class cannot access inner class private data members without being friend
+    std::cout<<"Hello , I'm the outer class A member function :) " <<endl;
+    b2.display(); // If the inner class object is declared inside outer class or after the {} parenthesis of inner class ends , then we need to call member functions of inner class objects through outer class member function objects.
+    // The above code will also work if the inner class is defined and declared in private visibility mode.
+    B b3; //You can also make objects of inner class inside outer class member functions.
+    b3.display();
+    A a6;
+
+}
+
+
+
 
 int main()
 {
-    cout << "=== NESTED CLASS COMPOSITION DEMO ===" << endl;
+    A::B b1;  //Make nested class objects like this ...
+    b1.display(); //It cannot use the member functions of outer class .
 
-    {
-        University mit("MIT", 1861);
-        mit.displayUniversity();
-        mit.letDepartmentsAccess();
 
-        cout << "\nLeaving block scope..." << endl;
-    } // ✅ Automatic destruction: University → Departments
-
-    cout << "Back in main" << endl;
+    A a1;
+    a1.fun();
+//    std::cout<<"Value of b's a is : "<<b1.a<<endl;
+//    std::cout<<"Value of A's a is : "<<a1.a<<endl;
+//    b1.a=100;
 
     return 0;
 }
+
+
+
