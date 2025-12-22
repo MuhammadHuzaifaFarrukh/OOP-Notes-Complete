71fb28fa46ebae45a16cf442096b75542b789d24
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2025-12-23 04:03:29 +0500

    Update main.cpp

--- 25) Ambiguity Resolution of Member Function in inheritance/main.cpp
+++ 25) Ambiguity Resolution of Member Function in inheritance/main.cpp
@@ -2,12 +2,20 @@
 
 using namespace std;
 
+//============== Member Name Hiding in MULTIPLE INHERITANCE ============
 //Only Member Functions having same name in 2 classes irrespective of parameters ,creates ambiguity when used in inheritance.
 //Same variables used in 2 different classes is allowed as well and do not create ambiguity . They are simply overwritten
 //or previous one can be used using scope resolution operator in some inheritances or by using keyword etc
 
-//Ambiguity in Multiple Inheritance
 
+//Ambiguity in Multiple Inheritance :
+//Here we observe Triangular conflict that is , same named member functions / variables when inherited in multiple
+//inheritance then this creates ambiguity to choose which one in the instantiated class
+//The Name of the function/variable is same but their purpose / implementation might be different in the classes that define them
+//When inherited in the lower hierarchy , compiler doesn't know which one to use as it sees two different functions/variables with same names
+
+//This can be solved by either scope resolution operator (::) or writing another member function / variable
+//that hides the other one , or by specifying the member function in another class by "using" keyword
 class base1
 {
 public:
@@ -35,11 +43,15 @@ private:
 protected:
 
 public:
+    //We can also solve this by writing :
+    //using base1::greet;
+    //However if you are doing "using" and still redefining the function/variable then this new redefined function/variable
+    //will hide the member that was specified by "using"
 
     //Below function can be avoided or not written if user has specified like in the main in this example
     void greet()
     {
-        base1::greet();   //Executes Base1 greet() function , also okay
+        base2::greet();   //Executes Base1 greet() function , also okay
     }
 };
 
@@ -55,6 +67,9 @@ int main()
 
     //Either above syntax or do resolution in the function like use only
     // obj3.greet() and write base2::greet() in the greet() function of obj3
+
+    //You can also write it like :
+    // obj3.greet() when you have specified the function by "using" keyword
 
     return 0;
 }
