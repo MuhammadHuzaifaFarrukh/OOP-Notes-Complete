c90faa0175bb98a478c37d4ef7910ee31ed6f7f1
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-01-26 00:13:38 +0500

    Delete Some Exercises directory

--- Some Exercises/2) Aggregation and Composition/10/Node.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include "Node.h"
-using namespace std;
-
-Node::Node(string m , Node *new_next) : message(m), next(new_next) {}
-string Node::getmessage() { return message; }
-Node* Node::getnextnode() { return next; }
