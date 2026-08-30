297bfbf0b2bf76c860ab167e3460c77582aeb375
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-08-30 19:15:47 +0500

    Update Abstract_Adapter_Skeletal_Implementation.cpp

--- 59) Adapter Class (Skeletal Implementation)/Abstract_Adapter_Skeletal_Implementation.cpp
+++ 59) Adapter Class (Skeletal Implementation)/Abstract_Adapter_Skeletal_Implementation.cpp
@@ -7,7 +7,9 @@ Skeletal Implementation (OOP Architectural Idiom in LLD)
 LLD Focus: Used when designing class hierarchies and custom libraries (e.g., defining an AbstractList base class that implements standard search/iteration methods, allowing derived classes like ArrayList or LinkedList to only implement core storage functions).
 Goal: Code reusability and minimizing boilerplate across concrete class implementations.
 Relies on Dynamic Polymorphism.
+This is the Default Abstract Adapter Design.
 */
+// Interface => Adapter (Abstract) => Concrete
 
 class A // This one will be Abstract Class (Interface Level)
 {
